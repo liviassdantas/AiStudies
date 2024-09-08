46e9481d74d325fdb7b4b1936c84587bbfb16a9d
Livia Dantas <livia.ss.dantas@outlook.com>
2024-09-08 20:26:44 -0300

    GPT respondendo

--- Program.cs
+++ Program.cs
@@ -1,6 +1,31 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Azure;
+using Azure.AI.OpenAI;
+using System;
+using static System.Environment;
 
-app.MapGet("/", () => "Hello World!");
+string endpoint = GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT", EnvironmentVariableTarget.User) ?? "";
+string key = GetEnvironmentVariable("AZURE_OPENAI_API_KEY", EnvironmentVariableTarget.User) ?? "";
+const string deploymentOrModelName = "EstudosIAGPT4";
+ async Task<string> GetContent(string prompt)
+{
+    var client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
 
-app.Run();
+    var chatCompletionsOptions = new ChatCompletionsOptions
+    {
+        DeploymentName = deploymentOrModelName,
+        Temperature = (float)0.5,
+        MaxTokens = 800,
+        NucleusSamplingFactor = (float)0.95,
+        FrequencyPenalty = 0,
+        PresencePenalty = 0,
+    };
+
+    chatCompletionsOptions.Messages.Add(new ChatRequestUserMessage(prompt));
+    var response = await client.GetChatCompletionsAsync(chatCompletionsOptions);
+
+    return response.Value.Choices[0].Message.Content;
+}
+
+var gptPrompt = "Gere uma frase motivacional para encarar a segunda-feira";
+
+Console.WriteLine($"{await GetContent(gptPrompt)}");
