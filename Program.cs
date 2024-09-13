551156383e9af80a1f60b9f47c80c1417b1b3553
Livia Dantas <livia.ss.dantas@outlook.com>
2024-09-13 08:40:35 -0300

    changing the class to work a part from program.cs

--- Program.cs
+++ Program.cs
@@ -1,31 +1,4 @@
-using Azure;
-using Azure.AI.OpenAI;
-using System;
-using static System.Environment;
-
-string endpoint = GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT", EnvironmentVariableTarget.User) ?? "";
-string key = GetEnvironmentVariable("AZURE_OPENAI_API_KEY", EnvironmentVariableTarget.User) ?? "";
-const string deploymentName = "EstudosIAGPT4";
- async Task<string> GetContent(string prompt)
-{
-    var client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
-
-    var chatCompletionsOptions = new ChatCompletionsOptions
-    {
-        DeploymentName = deploymentName,
-        Temperature = (float)0.5,
-        MaxTokens = 800,
-        NucleusSamplingFactor = (float)0.95,
-        FrequencyPenalty = 0,
-        PresencePenalty = 0,
-    };
-
-    chatCompletionsOptions.Messages.Add(new ChatRequestUserMessage(prompt));
-    var response = await client.GetChatCompletionsAsync(chatCompletionsOptions);
-
-    return response.Value.Choices[0].Message.Content;
-}
-
+using EstudosIA.Services;
 var gptPrompt = "Gere uma frase motivacional para encarar a segunda-feira";
 
-Console.WriteLine($"{await GetContent(gptPrompt)}");
+Console.WriteLine($"{await new AzureOpenAIClassService().GetContent(gptPrompt)}");
---
+++ Services/AzureOpenAIClassService.cs
@@ -0,0 +1,38 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using System;
+using static System.Environment;
+namespace EstudosIA.Services
+{
+    public class AzureOpenAIClassService
+    {
+
+        string endpoint = GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT", EnvironmentVariableTarget.User) ?? "";
+        string key = GetEnvironmentVariable("AZURE_OPENAI_API_KEY", EnvironmentVariableTarget.User) ?? "";
+        const string deploymentName = "EstudosIAGPT4";
+
+
+        public async Task<string> GetContent(string prompt)
+        {
+            var client = new OpenAIClient(new Uri(endpoint), new AzureKeyCredential(key));
+
+            var chatCompletionsOptions = new ChatCompletionsOptions
+            {
+                DeploymentName = deploymentName,
+                Temperature = (float)0.5,
+                MaxTokens = 400,
+                NucleusSamplingFactor = (float)0.95,
+                FrequencyPenalty = 0,
+                PresencePenalty = 0,
+            };
+
+            chatCompletionsOptions.Messages.Add(new ChatRequestUserMessage(prompt));
+            var response = await client.GetChatCompletionsAsync(chatCompletionsOptions);
+
+            return response.Value.Choices[0].Message.Content;
+        }
+
+    }
+}
+
+
